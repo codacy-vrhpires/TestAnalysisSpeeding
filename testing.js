d611ebf1d971d3793e06362b20168cbff355de7f
Vítor Pires <vitor.pires@codacy.com>
2023-06-28 12:14:30 +0100

    copying 3 files from pivot repository

---
+++ testing.js
@@ -0,0 +1,28 @@
+// some comment just to differentiate
+
+function a() {
+  console.log("asd" + 'dsa');
+}
+
+
+
+if(1 == "2" && "3" == 5) {
+  return;
+}
+
+var cenas = "asd132";
+
+if(true) d();
+
+var cemas = '123asd';
+a();
+
+b();
+
+var outrascenas = '123asd' + "321";
+var as= "321";
+
+
+
+var asd = '123asd' + "321";
+
